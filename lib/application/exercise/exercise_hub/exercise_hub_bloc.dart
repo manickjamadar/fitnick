2f3b160061535bf31b874fa28e730749a030746a
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-08 20:33:52 +0530

    exercise hub bloc init event implemented

--- lib/application/exercise/exercise_hub/exercise_hub_bloc.dart
+++ lib/application/exercise/exercise_hub/exercise_hub_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:fitnick/domain/core/unique_id.dart';
+import 'package:fitnick/domain/exercise/facade/i_exercise_facade.dart';
 import 'package:fitnick/domain/exercise/failure/exercise_failure.dart';
 import 'package:fitnick/domain/exercise/models/exercise.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -11,12 +12,27 @@ part 'exercise_hub_state.dart';
 part 'exercise_hub_bloc.freezed.dart';
 
 class ExerciseHubBloc extends Bloc<ExerciseHubEvent, ExerciseHubState> {
-  ExerciseHubBloc() : super(ExerciseHubState.loading());
+  final IExerciseFacade iExerciseFacade;
+  ExerciseHubBloc({@required this.iExerciseFacade})
+      : super(ExerciseHubState.loading());
 
   @override
   Stream<ExerciseHubState> mapEventToState(
     ExerciseHubEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* event.when(
+        init: _bindInitToState,
+        exerciseAdded: null,
+        exerciseUpdated: null,
+        exerciseDeleted: null,
+        exerciseReordered: null);
+  }
+
+  Stream<ExerciseHubState> _bindInitToState() async* {
+    final failureOrSuccess = await iExerciseFacade.findAll();
+    yield failureOrSuccess.fold(
+        (failure) => ExerciseHubState.loadedError(failure: failure),
+        (List<Exercise> exercises) =>
+            ExerciseHubState.loaded(exercise: exercises));
   }
 }
