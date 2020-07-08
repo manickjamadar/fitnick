eefca34073dabece80de440410e03a9f063fd900
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-08 15:28:14 +0530

    basic exercise facade created

---
+++ lib/domain/exercise/facade/i_exercise_facade.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fitnick/domain/exercise/failure/exercise_failure.dart';
+import 'package:fitnick/domain/exercise/models/exercise.dart';
+
+abstract class IExerciseFacade {
+  Future<Either<ExerciseFailure, List<Exercise>>> findAll();
+  Future<Either<ExerciseFailure, Unit>> createExercise(Exercise exercise);
+}
