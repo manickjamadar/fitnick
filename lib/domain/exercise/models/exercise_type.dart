c54e61882ec398c7054933e5648da0b4ecd253c7
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 13:48:01 +0530

    refactor exercise type and add some types

--- lib/domain/exercise/models/exercise_type.dart
+++ lib/domain/exercise/models/exercise_type.dart
@@ -1,19 +1,18 @@
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:fitnick/domain/exercise/models/exercise_target.dart';
 import 'package:flutter/foundation.dart';
 
 class ExerciseType extends Equatable {
   final String name;
-  final Option<List<ExerciseTarget>> targets;
 
-  const ExerciseType._({@required this.name, @required this.targets});
+  const ExerciseType._({@required this.name});
 
-  static const warmUp = ExerciseType._(name: "Warm Up", targets: None());
-  static const stretch =
-      ExerciseType._(name: "Warm Up", targets: Some([ExerciseTarget.triceps]));
+  static const warmUp = ExerciseType._(name: "Warm Up");
+  static const cardio = ExerciseType._(name: "Cardio");
+  static const strength = ExerciseType._(name: "Strength");
+  static const stretching = ExerciseType._(name: "Stretching");
+
   @override
-  List<Object> get props => [name, targets];
+  List<Object> get props => [name];
   @override
   String toString() {
     return "Exercise Type : $name";
