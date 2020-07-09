b98ca124eb28a16d46768912f6f3ec74dfaeb4e4
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 13:07:02 +0530

    execise hub bloc implmented in exercise tab

--- lib/presentation/screens/home/tabs/exercise_tab.dart
+++ lib/presentation/screens/home/tabs/exercise_tab.dart
@@ -1,8 +1,33 @@
+import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
+import 'package:fitnick/domain/exercise/models/exercise.dart';
+import 'package:fitnick/presentation/screens/home/widgets/exerise_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ExerciseTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Exercise Tab"));
+    return BlocBuilder<ExerciseHubBloc, ExerciseHubState>(
+      builder: (context, state) {
+        return state.when(
+          loading: () => buildLoading(),
+          loaded: (List<Exercise> exercises) => buildExercises(exercises),
+          loadedError: (_) => buildLoadedFailed(),
+          reorderedError: (List<Exercise> exercises, _) =>
+              buildExercises(exercises),
+        );
+      },
+    );
   }
+
+  Widget buildLoadedFailed() => Center(
+        child: Text("Exercise loading failed"),
+      );
+
+  Widget buildExercises(List<Exercise> exercises) =>
+      ExerciseListView(exercises: exercises);
+
+  Widget buildLoading() => Center(
+        child: CircularProgressIndicator(),
+      );
 }
