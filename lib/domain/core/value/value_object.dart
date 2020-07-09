1146e23bf76bd51f92ecff2b93d0a32f5c98b8ff
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 13:05:05 +0530

    add safe value in value object

--- lib/domain/core/value/value_object.dart
+++ lib/domain/core/value/value_object.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:fitnick/domain/core/errors.dart';
 
 import 'value_failure.dart';
 
@@ -6,4 +7,5 @@ abstract class ValueObject<T> {
   final Either<ValueFailure<T>, T> value;
   const ValueObject(this.value);
   bool get isValid => value.isRight();
+  T get safeValue => value.fold((l) => throw UnexpectedValueError(l), id);
 }
