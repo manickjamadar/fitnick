25b257d60250feb4b7bedd6316c924ed33c48ecd
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 12:38:05 +0530

    created my own defined icon data

---
+++ lib/presentation/core/my_icons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyIcons {
+  static const routine = Icons.group_work;
+  static const exercise = Icons.fitness_center;
+  static const progression = Icons.trending_up;
+}
