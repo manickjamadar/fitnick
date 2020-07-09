a9bc68cb53deb546f15c03c24ef0b77dff4a8294
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 13:06:47 +0530

    exercise list view widget created

---
+++ lib/presentation/screens/home/widgets/exerise_list_view.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:fitnick/domain/exercise/models/exercise.dart';
+import 'package:fitnick/presentation/screens/home/widgets/exercise_item.dart';
+import 'package:flutter/material.dart';
+
+class ExerciseListView extends StatelessWidget {
+  final List<Exercise> exercises;
+
+  const ExerciseListView({Key key, @required this.exercises}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return exercises.isEmpty ? buildEmptyExercise() : buildExerciseList();
+  }
+
+  ListView buildExerciseList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final exercise = exercises[index];
+        return ExerciseItem(
+          exercise: exercise,
+        );
+      },
+      itemCount: exercises.length,
+    );
+  }
+
+  Center buildEmptyExercise() => Center(
+        child: Text("No Exercise avaialble"),
+      );
+}
