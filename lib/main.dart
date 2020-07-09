68b3ce6029ffa2821e4d0bad3afbe28f0017bd7e
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 13:06:27 +0530

    exercise hub bloc added in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
 import 'package:fitnick/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'presentation/core/app.dart';
 
 void main() {
   initLocator();
-  runApp(App());
+  runApp(BlocProvider<ExerciseHubBloc>(
+      create: (_) => locator<ExerciseHubBloc>()..add(ExerciseHubEvent.init()),
+      child: App()));
 }
