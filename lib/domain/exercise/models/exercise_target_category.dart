aa06ae9440bc4bbf95f4f9382ef2b6e495df3560
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-09 13:23:05 +0530

    exercise target category created

---
+++ lib/domain/exercise/models/exercise_target_category.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+class ExerciseTargetCategory extends Equatable {
+  final String name;
+  const ExerciseTargetCategory._({@required this.name});
+
+  static const upperBody = ExerciseTargetCategory._(name: "Upper Body");
+  static const lowerBody = ExerciseTargetCategory._(name: "Lower Body");
+  static const core = ExerciseTargetCategory._(name: "Core");
+  @override
+  List<Object> get props => [name];
+  @override
+  String toString() {
+    return "Exercise Type Category : $name";
+  }
+}
