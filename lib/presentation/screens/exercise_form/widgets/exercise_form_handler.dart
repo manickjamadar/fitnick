b2e113a8ce095a086844499cf349e8932f89f1e9
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-10 06:48:03 +0530

    basic add exercise form ui created

--- lib/presentation/screens/exercise_form/widgets/exercise_form_handler.dart
+++ lib/presentation/screens/exercise_form/widgets/exercise_form_handler.dart
@@ -1,20 +1,71 @@
+import 'package:fitnick/application/exercise/exercise_form/exercise_form_bloc.dart';
+import 'package:fitnick/domain/exercise/models/exercise_level.dart';
+import 'package:fitnick/domain/exercise/models/exercise_target.dart';
+import 'package:fitnick/domain/exercise/models/exercise_tool.dart';
+import 'package:fitnick/domain/exercise/models/exercise_type.dart';
 import 'package:fitnick/presentation/screens/exercise_form/widgets/selector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ExerciseFormHandler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        TextField(),
-        Selector(
-          label: "Level",
-        ),
-        Text("Tool"),
-        Text("Type"),
-        Text("Target"),
-      ],
+    return BlocBuilder<ExerciseFormBloc, ExerciseFormState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              buildNameInput(),
+              buildSpace(),
+              Selector<ExerciseLevel>(
+                initialValue: state.exercise.level,
+                label: "Level",
+                options: ExerciseLevel.all,
+              ),
+              Selector<ExerciseTool>(
+                initialValue: state.exercise.tool,
+                label: "Tool",
+                options: ExerciseTool.all,
+              ),
+              Selector<ExerciseType>(
+                initialValue: state.exercise.type,
+                label: "Type",
+                options: ExerciseType.all,
+              ),
+              Selector<ExerciseTarget>(
+                initialValue: state.exercise.target,
+                label: "Muscle Target",
+                options: ExerciseTarget.all,
+              ),
+              Spacer(),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                  child: Text("Add"),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  onPressed: () {},
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildNameInput() {
+    return TextField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(), labelText: "Exercise Name"),
+    );
+  }
+
+  Widget buildSpace() {
+    return SizedBox(
+      height: 20,
     );
   }
 }
