240f3c5ef70df479a9aa2097136b82f7163b0cb3
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-10 15:28:04 +0530

    local exercise facade basica implementation created

---
+++ lib/infrastructure/exercise/facade/local_exercise_facade.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:fitnick/domain/exercise/facade/i_exercise_facade.dart';
+import 'package:fitnick/domain/exercise/models/exercise.dart';
+import 'package:fitnick/domain/exercise/failure/exercise_failure.dart';
+import 'package:fitnick/infrastructure/exercise/data_source/i_exercise_data_source.dart';
+import 'package:fitnick/infrastructure/exercise/entities/exercise_entity.dart';
+import 'package:flutter/foundation.dart';
+
+class LocalExerciseFacade extends IExerciseFacade {
+  final IExerciseDataSource dataSource;
+
+  LocalExerciseFacade({@required this.dataSource});
+  @override
+  Future<Either<ExerciseFailure, Unit>> createExercise(
+      Exercise exercise) async {
+    try {
+      await dataSource.addExercise(ExerciseEntity.fromModel(exercise));
+      return Right(unit);
+    } catch (error) {
+      assert(() {
+        print(error);
+        return true;
+      }());
+      return Left(ExerciseFailure.create());
+    }
+  }
+
+  @override
+  Future<Either<ExerciseFailure, List<Exercise>>> findAll() async {
+    try {
+      final exerciseEntities = await dataSource.finalAll();
+      return Right(exerciseEntities.map((entity) => entity.toModel()).toList());
+    } catch (error) {
+      assert(() {
+        print(error);
+        return true;
+      }());
+      return Left(ExerciseFailure.find());
+    }
+  }
+
+  @override
+  Future<Either<ExerciseFailure, Unit>> updateExercise(Exercise exercise) {
+    // TODO: implement updateExercise
+    throw UnimplementedError();
+  }
+}
