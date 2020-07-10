cbb56a3e76debcc52a8dc0dfad951f46516b3b71
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-10 11:15:54 +0530

    create name json serializer

--- lib/domain/exercise/models/name.dart
+++ lib/domain/exercise/models/name.dart
@@ -1,10 +1,15 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+part "name.g.dart";
 
+@JsonSerializable()
 class Name extends Equatable {
   final String name;
 
   const Name(this.name);
 
+  factory Name.fromJson(Map<String, dynamic> json) => _$NameFromJson(json);
+  Map<String, dynamic> toJson() => _$NameToJson(this);
   @override
   List<Object> get props => [name];
 }
