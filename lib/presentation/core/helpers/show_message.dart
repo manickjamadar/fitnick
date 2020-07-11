2d3e892d8877acccb11e71fb07516d776eda4220
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-11 15:28:11 +0530

    show message snackbar created

---
+++ lib/presentation/core/helpers/show_message.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+abstract class MessageType {
+  Color get color;
+}
+
+class WarningMessage extends MessageType {
+  @override
+  Color get color => Colors.yellow;
+}
+
+class ErrorMessage extends MessageType {
+  @override
+  Color get color => Colors.red;
+}
+
+class SuccessMessage extends MessageType {
+  @override
+  Color get color => Colors.green;
+}
+
+class InfoMessage extends MessageType {
+  @override
+  Color get color => Colors.blue;
+}
+
+void showMessage(BuildContext context,
+    {@required String message, @required MessageType type}) {
+  final scaffold = Scaffold.of(context);
+  scaffold.hideCurrentSnackBar();
+  scaffold.showSnackBar(SnackBar(
+      content: Text(message, style: TextStyle(color: Colors.white)),
+      backgroundColor: type.color));
+}
