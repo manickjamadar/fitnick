a8da20bdc52620ebf8976fcbbee392ec8f912b4f
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-11 16:06:31 +0530

    create updatet exercise method in data source abstraction

--- lib/infrastructure/exercise/data_source/i_exercise_data_source.dart
+++ lib/infrastructure/exercise/data_source/i_exercise_data_source.dart
@@ -1,8 +1,9 @@
-import 'package:fitnick/domain/core/unique_id.dart';
-import 'package:fitnick/infrastructure/exercise/entities/exercise_entity.dart';
+import '../../../domain/core/unique_id.dart';
+import '../entities/exercise_entity.dart';
 
 abstract class IExerciseDataSource {
   Future<void> addExercise(ExerciseEntity exerciseEntity);
   Future<List<ExerciseEntity>> finalAll();
   Future<void> deleteExercise(UniqueId exerciseId);
+  Future<void> updateExercise(ExerciseEntity exerciseEntity);
 }
