cb3239aab92d2a4679964a021a4103efa4e5eea5
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-11 09:48:41 +0530

    add slidable delete and edit in exercise item

--- lib/presentation/screens/home/widgets/exercise_item.dart
+++ lib/presentation/screens/home/widgets/exercise_item.dart
@@ -1,5 +1,6 @@
 import 'package:fitnick/domain/exercise/models/exercise.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import "../../../core/helpers/string_extension.dart";
 
 class ExerciseItem extends StatelessWidget {
@@ -8,28 +9,60 @@ class ExerciseItem extends StatelessWidget {
   const ExerciseItem({Key key, @required this.exercise}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return exercise.failureOption().fold(
-        () => Card(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ListTile(
-                  leading: CircleAvatar(child: Icon(Icons.fitness_center)),
-                  title: Text(
-                    exercise.name.safeValue.capitalize(),
-                  ),
-                  subtitle: Text(
-                      "${exercise.level.name} , ${exercise.tool.name} , ${exercise.type.name}"),
-                  trailing: Text(exercise.target.name),
-                ),
-              ),
-            ),
-        (a) => Container(
-              color: Colors.red,
-              height: 30,
-              child: Text(
-                "Exercise Item Error",
-                style: TextStyle(color: Colors.white),
-              ),
-            ));
+    return exercise
+        .failureOption()
+        .fold(() => buildExerciseItem(), (a) => buildExerciseErrorCard());
+  }
+
+  Container buildExerciseErrorCard() {
+    return Container(
+      color: Colors.red,
+      height: 30,
+      child: Text(
+        "Exercise Item Error",
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  Widget buildExerciseItem() {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      child: buildExerciseCard(),
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          caption: 'Edit',
+          color: Colors.blue,
+          icon: Icons.edit,
+          onTap: () {},
+        ),
+        IconSlideAction(
+          caption: 'Delete',
+          color: Colors.red,
+          icon: Icons.delete,
+          onTap: () {},
+        ),
+      ],
+    );
+  }
+
+  Widget buildExerciseCard() {
+    return Card(
+      margin: EdgeInsets.all(0),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+        // decoration: BoxDecoration(border: Border.all()),
+        child: ListTile(
+          leading: CircleAvatar(child: Icon(Icons.fitness_center)),
+          title: Text(
+            exercise.name.safeValue.capitalize(),
+          ),
+          subtitle: Text(
+              "${exercise.level.name} , ${exercise.tool.name} , ${exercise.type.name}"),
+          trailing: Text(exercise.target.name),
+        ),
+      ),
+    );
   }
 }
