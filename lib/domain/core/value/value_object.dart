aefff09956a79704a3a1990b02494a9f6d34fae2
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-12 15:14:51 +0530

    add equality to value object

--- lib/domain/core/value/value_object.dart
+++ lib/domain/core/value/value_object.dart
@@ -1,12 +1,15 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:fitnick/domain/core/errors.dart';
 
 import 'value_failure.dart';
 
-abstract class ValueObject<T> {
+abstract class ValueObject<T> extends Equatable {
   final Either<ValueFailure<T>, T> value;
   const ValueObject(this.value);
   bool get isValid => value.isRight();
   T get safeValue => value.fold((l) => throw UnexpectedValueError(l), id);
   T get dangerValue => value.fold((l) => l.invalidValue, id);
+  @override
+  List<Object> get props => [dangerValue];
 }
