dd331da28daef1ed2942427a9e614dc66e57e577
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-12 15:29:48 +0530

    workout entity model conversion test created

--- test/workout_entity_test.dart
+++ test/workout_entity_test.dart
@@ -1,4 +1,7 @@
 import 'package:fitnick/domain/core/unique_id.dart';
+import 'package:fitnick/domain/exercise/models/exercise.dart';
+import 'package:fitnick/domain/workout/models/workout.dart';
+import 'package:fitnick/domain/workout/value_object/workout_name.dart';
 import 'package:fitnick/infrastructure/workout/entities/workout_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -19,6 +22,58 @@ void main() {
       final newWorkoutEntity =
           WorkoutEntity.fromLocalJson(workoutEntity.toLocalJson());
       expect(workoutEntity, equals(newWorkoutEntity));
+    });
+    test("from model conversion with empty exercises", () {
+      final workoutId = UniqueId();
+      final workoutName = "new workout";
+      final workout =
+          Workout(id: workoutId, name: WorkoutName(workoutName), exercises: []);
+      final expectedWorkoutEntity =
+          WorkoutEntity(id: workoutId, name: workoutName, exerciseIds: []);
+      expect(WorkoutEntity.fromModel(workout), equals(expectedWorkoutEntity));
+    });
+    test("from model conversion with loaded exercises", () {
+      final exerciseIds = [UniqueId(), UniqueId(), UniqueId()];
+      final List<Exercise> exercises = [
+        Exercise.initial().copyWith(id: exerciseIds[0]),
+        Exercise.initial().copyWith(id: exerciseIds[2]),
+      ];
+      final workoutId = UniqueId();
+      final workoutName = "new workout";
+      final workout = Workout(
+          id: workoutId, name: WorkoutName(workoutName), exercises: exercises);
+      final expectedWorkoutEntity = WorkoutEntity(
+          id: workoutId,
+          name: workoutName,
+          exerciseIds: [exerciseIds[0], exerciseIds[2]]);
+      expect(WorkoutEntity.fromModel(workout), expectedWorkoutEntity);
+    });
+    test("to model conversion with empty exercises", () {
+      final List<Exercise> exercises = [];
+      final workoutId = UniqueId();
+      final workoutName = "new workout";
+      final workoutEntity = WorkoutEntity(
+          id: workoutId, name: workoutName, exerciseIds: [UniqueId()]);
+      final expectedWorkout =
+          Workout(id: workoutId, name: WorkoutName(workoutName), exercises: []);
+      expect(workoutEntity.toModel(exercises), expectedWorkout);
+    });
+    test("to model conversion with loaded exercises", () {
+      final exerciseIds = [UniqueId(), UniqueId(), UniqueId()];
+      final List<Exercise> exercises = [
+        Exercise.initial().copyWith(id: exerciseIds[0]),
+        Exercise.initial().copyWith(id: exerciseIds[1]),
+        Exercise.initial().copyWith(id: exerciseIds[2]),
+      ];
+      final workoutId = UniqueId();
+      final workoutName = "new workout";
+      final workoutEntity = WorkoutEntity(
+          id: workoutId, name: workoutName, exerciseIds: [exerciseIds[1]]);
+      final expectedWorkout = Workout(
+          id: workoutId,
+          name: WorkoutName(workoutName),
+          exercises: [exercises[1]]);
+      expect(workoutEntity.toModel(exercises), expectedWorkout);
     });
   });
 }
