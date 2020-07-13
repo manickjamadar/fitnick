85157e64e244e82ea6f200bcbe66216cf7da44ef
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-13 15:50:01 +0530

    exercise item type implemented in exercise item

--- lib/presentation/screens/home/widgets/exercise/exercise_item.dart
+++ lib/presentation/screens/home/widgets/exercise/exercise_item.dart
@@ -4,6 +4,7 @@ import 'package:fitnick/domain/exercise/models/exercise.dart';
 import 'package:fitnick/presentation/core/helpers/show_message.dart';
 import 'package:fitnick/presentation/core/widgets/error_card.dart';
 import 'package:fitnick/presentation/screens/exercise_form/exercise_form_screen.dart';
+import 'package:fitnick/presentation/screens/home/widgets/exercise/exercise_item_type.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
@@ -11,8 +12,12 @@ import '../../../../core/helpers/string_extension.dart';
 
 class ExerciseItem extends StatelessWidget {
   final Exercise exercise;
-
-  const ExerciseItem({Key key, @required this.exercise}) : super(key: key);
+  final ExerciseItemType exerciseItemType;
+  const ExerciseItem(
+      {Key key,
+      @required this.exercise,
+      this.exerciseItemType = const ExerciseItemType.normal()})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return exercise.failureOption().fold(
@@ -30,7 +35,7 @@ class ExerciseItem extends StatelessWidget {
       key: ValueKey(exercise.id.value),
       actionPane: SlidableDrawerActionPane(),
       actionExtentRatio: 0.25,
-      child: buildExerciseCard(),
+      child: buildExerciseCard(context),
       secondaryActions: <Widget>[
         IconSlideAction(
           caption: 'Edit',
@@ -61,23 +66,68 @@ class ExerciseItem extends StatelessWidget {
     );
   }
 
-  Widget buildExerciseCard() {
-    return Card(
-      margin: EdgeInsets.all(0),
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-        // decoration: BoxDecoration(border: Border.all()),
-        child: ListTile(
-          leading: CircleAvatar(child: Icon(Icons.fitness_center)),
-          title: Text(
-            exercise.name.safeValue.capitalize(),
+  Widget buildExerciseCard(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: exerciseItemType.maybeWhen(
+                orElse: () => Colors.transparent,
+                selectable: (onSelect, selected) {
+                  return selected
+                      ? Theme.of(context).primaryColor
+                      : Colors.transparent;
+                },
+              ),
+              width: 3),
+          borderRadius: BorderRadius.circular(6)),
+      child: Card(
+        margin: EdgeInsets.all(0),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+          // decoration: BoxDecoration(border: Border.all()),
+          child: ListTile(
+            leading: CircleAvatar(child: Icon(Icons.fitness_center)),
+            title: Text(
+              exercise.name.safeValue.capitalize(),
+            ),
+            subtitle: Text(exercise.target.name),
+            trailing: buildCardTrailing(context),
           ),
-          subtitle: Text(
-              "${exercise.level.name} , ${exercise.tool.name} , ${exercise.type.name}"),
-          trailing: Text(exercise.target.name),
         ),
       ),
     );
+  }
+
+  Widget buildCardTrailing(BuildContext context) {
+    return exerciseItemType.when(
+        normal: () => null,
+        removable: (onRemove) {
+          return IconButton(
+            onPressed: () {
+              if (onRemove != null) {
+                onRemove(exercise);
+              }
+            },
+            icon: Icon(
+              Icons.clear,
+              color: Colors.red,
+            ),
+          );
+        },
+        selectable: (onSelect, selected) {
+          return IconButton(
+              onPressed: () {
+                if (onSelect != null) {
+                  onSelect(selected);
+                }
+              },
+              icon: Icon(
+                Icons.check_box,
+                color: selected
+                    ? Theme.of(context).primaryColor
+                    : Colors.grey[400],
+              ));
+        });
   }
 
   void _onExerciseDelete(BuildContext context, Exercise exercise) {
