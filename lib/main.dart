ff9c79f6dfeca6f7dce01453af9c7dcbf072a2e5
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-13 10:36:58 +0530

    workout hub bloc added in main app as a provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart
 import 'package:fitnick/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'application/workout/workout_hub/workout_hub_bloc.dart';
 import 'presentation/core/app.dart';
 
 void main() async {
@@ -12,6 +13,9 @@ void main() async {
     providers: [
       BlocProvider<ExerciseHubBloc>(
         create: (_) => locator<ExerciseHubBloc>()..add(ExerciseHubEvent.init()),
+      ),
+      BlocProvider<WorkoutHubBloc>(
+        create: (_) => locator<WorkoutHubBloc>()..add(WorkoutHubEvent.init()),
       ),
       BlocProvider<ExerciseActorBloc>(
         create: (_) => locator<ExerciseActorBloc>(),
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:fitnick/application/exercise/exercise_actor/exercise_actor_bloc.dart';
 import 'package:fitnick/application/exercise/exercise_form/exercise_form_bloc.dart';
 import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
+import 'package:fitnick/application/workout/workout_hub/workout_hub_bloc.dart';
 import 'package:fitnick/domain/exercise/facade/i_exercise_facade.dart';
 import 'package:fitnick/external/local_databse_provider.dart';
 import 'package:fitnick/infrastructure/exercise/data_source/i_exercise_data_source.dart';
@@ -51,4 +52,6 @@ void initBloc() {
   locator.registerFactory<ExerciseActorBloc>(() => ExerciseActorBloc(
         exerciseFacade: locator<IExerciseFacade>(),
       ));
+  locator.registerFactory<WorkoutHubBloc>(
+      () => WorkoutHubBloc(workoutFacade: locator<IWorkoutFacade>()));
 }
