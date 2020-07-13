f59febf1e4bf1d640b34df2d46b69ee846449e2c
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-13 10:33:39 +0530

    workout hub bloc event logic implemented

--- lib/application/workout/workout_hub/workout_hub_bloc.dart
+++ lib/application/workout/workout_hub/workout_hub_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:fitnick/domain/workout/facade/i_workout_facade.dart';
 import 'package:fitnick/domain/workout/failure/workout_failure.dart';
 import 'package:fitnick/domain/workout/models/workout.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -10,7 +11,9 @@ part 'workout_hub_state.dart';
 part "workout_hub_bloc.freezed.dart";
 
 class WorkoutHubBloc extends Bloc<WorkoutHubEvent, WorkoutHubState> {
-  WorkoutHubBloc() : super(WorkoutHubState.loading());
+  final IWorkoutFacade workoutFacade;
+  WorkoutHubBloc({@required this.workoutFacade})
+      : super(WorkoutHubState.loading());
 
   @override
   Stream<WorkoutHubState> mapEventToState(
@@ -19,6 +22,15 @@ class WorkoutHubBloc extends Bloc<WorkoutHubEvent, WorkoutHubState> {
     yield* event.when(init: _mapInitToState, refreshed: _mapRefreshedToState);
   }
 
-  Stream<WorkoutHubState> _mapInitToState() async* {}
-  Stream<WorkoutHubState> _mapRefreshedToState() async* {}
+  Stream<WorkoutHubState> _mapInitToState() async* {
+    yield WorkoutHubState.loading();
+    yield* _mapRefreshedToState();
+  }
+
+  Stream<WorkoutHubState> _mapRefreshedToState() async* {
+    final failureOrSuccess = await workoutFacade.findAll();
+    yield failureOrSuccess.fold(
+        (failure) => WorkoutHubState.loadedError(failure: failure),
+        (List<Workout> workouts) => WorkoutHubState.loaded(workouts: workouts));
+  }
 }
