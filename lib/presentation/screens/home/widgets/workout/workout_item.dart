8fc546a87ad25bfa4c8be3995f147ec68d4fd1b0
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-13 10:52:06 +0530

    basic workout item created

---
+++ lib/presentation/screens/home/widgets/workout/workout_item.dart
@@ -0,0 +1,34 @@
+import 'package:fitnick/domain/workout/models/workout.dart';
+import 'package:fitnick/presentation/core/widgets/error_card.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutItem extends StatelessWidget {
+  final Workout workout;
+
+  const WorkoutItem({Key key, @required this.workout}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return workout
+        .failureOption()
+        .fold(() => buildWorkoutCard(), (a) => buildWorkoutErrorCard());
+  }
+
+  Widget buildWorkoutCard() {
+    return Card(
+      child: ListTile(
+        title: Text(workout.name.safeValue),
+        subtitle: Text("${_getExerciseCount(workout)} exercises"),
+      ),
+    );
+  }
+
+  Widget buildWorkoutErrorCard() {
+    return ErrorCard(
+      title: "Workout Item Error",
+    );
+  }
+
+  int _getExerciseCount(Workout workout) {
+    return workout.exercises.length;
+  }
+}
