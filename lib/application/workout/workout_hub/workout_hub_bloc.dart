37dc9da165d0db2b6ee74bf4b3e554131b9083ad
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-16 11:18:02 +0530

    added exercise hub bloc dependency to workout hub bloc

--- lib/application/workout/workout_hub/workout_hub_bloc.dart
+++ lib/application/workout/workout_hub/workout_hub_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
+import 'package:fitnick/domain/exercise/models/exercise.dart';
 import 'package:fitnick/domain/workout/facade/i_workout_facade.dart';
 import 'package:fitnick/domain/workout/failure/workout_failure.dart';
 import 'package:fitnick/domain/workout/models/workout.dart';
@@ -11,9 +13,18 @@ part 'workout_hub_state.dart';
 part "workout_hub_bloc.freezed.dart";
 
 class WorkoutHubBloc extends Bloc<WorkoutHubEvent, WorkoutHubState> {
+  final ExerciseHubBloc exerciseHubBloc;
   final IWorkoutFacade workoutFacade;
-  WorkoutHubBloc({@required this.workoutFacade})
-      : super(WorkoutHubState.loading());
+  WorkoutHubBloc({@required this.workoutFacade, @required this.exerciseHubBloc})
+      : super(WorkoutHubState.loading()) {
+    exerciseHubBloc.listen((ExerciseHubState state) {
+      state.maybeWhen(
+          orElse: () {},
+          loaded: (List<Exercise> exercise) {
+            this.add(WorkoutHubEvent.refreshed());
+          });
+    });
+  }
 
   @override
   Stream<WorkoutHubState> mapEventToState(
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:fitnick/application/exercise/exercise_actor/exercise_actor_bloc.dart';
 import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
+import 'package:fitnick/domain/workout/failure/workout_failure.dart';
 import 'package:fitnick/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'application/workout/workout_actor/workout_actor_bloc.dart';
 import 'application/workout/workout_hub/workout_hub_bloc.dart';
+import 'domain/workout/facade/i_workout_facade.dart';
 import 'presentation/core/app.dart';
 
 void main() async {
@@ -19,7 +21,10 @@ void main() async {
         create: (_) => locator<ExerciseActorBloc>(),
       ),
       BlocProvider<WorkoutHubBloc>(
-        create: (_) => locator<WorkoutHubBloc>()..add(WorkoutHubEvent.init()),
+        create: (ctx) => WorkoutHubBloc(
+            workoutFacade: locator<IWorkoutFacade>(),
+            exerciseHubBloc: BlocProvider.of<ExerciseHubBloc>(ctx))
+          ..add(WorkoutHubEvent.init()),
       ),
       BlocProvider<WorkoutActorBloc>(
         create: (_) => locator<WorkoutActorBloc>(),
