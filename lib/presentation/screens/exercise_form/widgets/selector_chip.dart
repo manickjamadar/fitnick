d32a5ab147bfb943e22a6ed713b16204571ea18d
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-16 19:00:40 +0530

    basic selector chip created

---
+++ lib/presentation/screens/exercise_form/widgets/selector_chip.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SelectorChip extends StatelessWidget {
+  final String title;
+  final Color color;
+  final bool selected;
+  final void Function() onTap;
+  const SelectorChip(
+      {Key key,
+      @required this.title,
+      this.color = Colors.black,
+      this.onTap,
+      this.selected = false})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          decoration: BoxDecoration(
+              color: selected ? color : Colors.transparent,
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all()),
+          child: Text(title,
+              style: TextStyle(color: selected ? Colors.white : color))),
+    );
+  }
+}
