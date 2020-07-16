abb5b669a0dd47aa267a10d751e810559f2db9da
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-16 14:35:18 +0530

    new exercise tool added

--- lib/domain/exercise/models/sub_models/exercise_tool.dart
+++ lib/domain/exercise/models/sub_models/exercise_tool.dart
@@ -9,11 +9,31 @@ part 'exercise_tool.g.dart';
 class ExerciseTool extends Name {
   const ExerciseTool({@required String name}) : super(name);
 
-  static const bodyWeight = ExerciseTool(name: "Body Weight");
-  static const pushUpBar = ExerciseTool(name: "Push Up Bar");
+  static const bodyWeight = ExerciseTool(name: "No Equipment");
+  static const parallettes = ExerciseTool(name: "Parallettes");
   static const miniBand = ExerciseTool(name: "Mini Band");
+  static const dumbbell = ExerciseTool(name: "Dumbbell");
+  static const barbell = ExerciseTool(name: "Barbell");
+  static const dipBar = ExerciseTool(name: "Dip Bar");
+  static const rings = ExerciseTool(name: "Rings");
+  static const pullUpBar = ExerciseTool(name: "Pull Up Bar");
+  static const weightedVest = ExerciseTool(name: "Weighted Vest");
+  static const resistanceTube = ExerciseTool(name: "Resistance Tube");
+  static const medicineBall = ExerciseTool(name: "Medicine Ball");
 
-  static const all = [bodyWeight, pushUpBar, miniBand];
+  static const all = [
+    bodyWeight,
+    parallettes,
+    miniBand,
+    dumbbell,
+    barbell,
+    dipBar,
+    rings,
+    pullUpBar,
+    weightedVest,
+    resistanceTube,
+    medicineBall
+  ];
   factory ExerciseTool.fromJson(Map<String, dynamic> json) =>
       _$ExerciseToolFromJson(json);
   Map<String, dynamic> toJson() => _$ExerciseToolToJson(this);
