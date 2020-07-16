594eae906c45bb9a0692968bd3f45edeef9bc280
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-16 20:21:56 +0530

    add some exercise info in exercise runner viewer

--- lib/presentation/screens/workout_running_screen/widgets/exercise_runner_viewer.dart
+++ lib/presentation/screens/workout_running_screen/widgets/exercise_runner_viewer.dart
@@ -1,5 +1,7 @@
 import 'package:fitnick/domain/exercise/models/exercise.dart';
+import 'package:fitnick/domain/exercise/models/sub_models/name.dart';
 import 'package:flutter/material.dart';
+import '../../../core/helpers/string_extension.dart';
 
 class ExerciseRunnerViewer extends StatelessWidget {
   final Exercise exercise;
@@ -9,14 +11,45 @@ class ExerciseRunnerViewer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(exercise.name.safeValue.toUpperCase(),
+        Text(exercise.name.safeValue.capitalize(),
             style: TextStyle(
                 color: Colors.indigo,
-                fontSize: 40,
+                fontSize: 30,
                 fontWeight: FontWeight.bold)),
         buildSpace(),
+        buildTitle("Level"),
+        buildOptionName(exercise.levels.first),
+        buildTitle("Tools"),
+        buildMultipleOption(exercise.tools),
+        buildTitle("Type"),
+        buildMultipleOption(exercise.types),
+        buildTitle("Muscle Involved"),
+        buildMultipleOption(
+            [...exercise.primaryTargets, ...exercise.secondaryTargets])
       ],
+    );
+  }
+
+  Widget buildMultipleOption<T extends Name>(List<T> options) {
+    return Container(
+      height: 60,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: options.map((o) => buildOptionName(o)).toList(),
+      ),
+    );
+  }
+
+  Widget buildOptionName<T extends Name>(T option,
+      {Color color = Colors.blue}) {
+    return Container(
+      child: Text(option.name, style: TextStyle(color: Colors.white)),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(4), color: color),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
     );
   }
 
