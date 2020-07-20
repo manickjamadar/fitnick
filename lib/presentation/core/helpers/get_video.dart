25001dd9f0d6fb8d08b23ef463eb1bf4f434f575
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-20 07:55:55 +0530

    changed get video from camera to gallery

--- lib/presentation/core/helpers/get_video.dart
+++ lib/presentation/core/helpers/get_video.dart
@@ -1,5 +1,5 @@
 import 'package:image_picker/image_picker.dart';
 
 Future<PickedFile> pickNewVideo() async {
-  return ImagePicker().getVideo(source: ImageSource.camera);
+  return ImagePicker().getVideo(source: ImageSource.gallery);
 }
