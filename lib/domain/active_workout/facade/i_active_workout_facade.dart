810f32899d3948a89c419ce6bf6db6c3cba048ed
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-26 13:49:13 +0530

    fix name of active workout facade interface

--- lib/domain/active_workout/facade/i_active_workout_facade.dart
+++ lib/domain/active_workout/facade/i_active_workout_facade.dart
@@ -3,7 +3,7 @@ import 'package:fitnick/domain/active_workout/models/active_workout.dart';
 import 'package:fitnick/domain/core/unique_id.dart';
 import 'package:fitnick/domain/workout/failure/workout_failure.dart';
 
-abstract class ActiveWorkoutFacade {
+abstract class IActiveWorkoutFacade {
   Future<Either<WorkoutFailure, List<ActiveWorkout>>> findAll();
   Future<Either<WorkoutFailure, Unit>> create(ActiveWorkout activeWorkout);
   Future<Either<WorkoutFailure, Unit>> update(ActiveWorkout activeWorkout);
