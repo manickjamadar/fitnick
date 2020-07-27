0b3d9f657a6c77b526a36b4aa4a7b89ad33359a2
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-27 13:26:06 +0530

    active workout hub registered in main and locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fitnick/application/exercise/exercise_actor/exercise_actor_bloc.dart';
 import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
+import 'package:fitnick/domain/active_workout/models/active_workout.dart';
 import 'package:fitnick/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,6 +8,7 @@ import 'application/workout/workout_actor/workout_actor_bloc.dart';
 import 'application/workout/workout_hub/workout_hub_bloc.dart';
 import 'domain/workout/facade/i_workout_facade.dart';
 import 'presentation/core/app.dart';
+import "application/active_workout/active_workout_hub/cubit/active_workout_hub_cubit.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,6 +27,8 @@ void main() async {
             exerciseHubBloc: BlocProvider.of<ExerciseHubBloc>(ctx))
           ..add(WorkoutHubEvent.init()),
       ),
+      BlocProvider<ActiveWorkoutHubCubit>(
+          create: (_) => locator<ActiveWorkoutHubCubit>()),
       BlocProvider<WorkoutActorBloc>(
         create: (_) => locator<WorkoutActorBloc>(),
       ),
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,8 +4,15 @@ import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart
 import 'package:fitnick/application/workout/workout_form/workout_form_bloc.dart';
 import 'package:fitnick/application/workout/workout_hub/workout_hub_bloc.dart';
 import 'package:fitnick/application/workout/workout_running/workout_running_bloc.dart';
+import 'package:fitnick/domain/active_exercise/facade/i_active_exercise_facade.dart';
+import 'package:fitnick/domain/active_workout/facade/i_active_workout_facade.dart';
 import 'package:fitnick/domain/exercise/facade/i_exercise_facade.dart';
 import 'package:fitnick/external/local_databse_provider.dart';
+import 'package:fitnick/infrastructure/active_exercise/data_source/i_active_exercise_data_source.dart';
+import 'package:fitnick/infrastructure/active_exercise/facade/active_exercise_facade.dart';
+import 'package:fitnick/infrastructure/active_workout/data_source/i_active_workout_data_source.dart';
+import 'package:fitnick/infrastructure/active_workout/data_source/local_active_workout_data_source.dart';
+import 'package:fitnick/infrastructure/active_workout/facade/active_workout_facade.dart';
 import 'package:fitnick/infrastructure/exercise/data_source/i_exercise_data_source.dart';
 import 'package:fitnick/infrastructure/exercise/data_source/local_exercise_data_source.dart';
 import 'package:fitnick/infrastructure/exercise/facade/local_exercise_facade.dart';
@@ -14,9 +21,10 @@ import 'package:fitnick/infrastructure/workout/data_source/local_workout_data_so
 import 'package:fitnick/infrastructure/workout/facade/local_workout_facade.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sqflite/sqflite.dart';
-
+import "application/active_workout/active_workout_hub/cubit/active_workout_hub_cubit.dart";
 import 'application/workout/workout_actor/workout_actor_bloc.dart';
 import 'domain/workout/facade/i_workout_facade.dart';
+import 'infrastructure/active_exercise/data_source/local_active_exercise_data_source.dart';
 
 final locator = GetIt.instance;
 
@@ -37,6 +45,10 @@ void initDataSource() {
       () => LocalExerciseDataSource(database: locator<Database>()));
   locator.registerLazySingleton<IWorkoutDataSource>(
       () => LocalWorkoutDataSource(database: locator<Database>()));
+  locator.registerLazySingleton<IActiveWorkoutDataSource>(
+      () => LocalActiveWorkoutDataSource(database: locator<Database>()));
+  locator.registerLazySingleton<IActiveExerciseDataSource>(
+      () => LocalActiveExerciseDataSource(database: locator<Database>()));
 }
 
 void initFacade() {
@@ -45,6 +57,14 @@ void initFacade() {
   locator.registerLazySingleton<IWorkoutFacade>(() => LocalWorkoutFacade(
       exerciseFacade: locator<IExerciseFacade>(),
       workoutDataSource: locator<IWorkoutDataSource>()));
+  locator.registerLazySingleton<IActiveExerciseFacade>(
+    () => ActiveExerciseFacade(
+        exerciseFacade: locator<IExerciseFacade>(),
+        dataSource: locator<IActiveExerciseDataSource>()),
+  );
+  locator.registerLazySingleton<IActiveWorkoutFacade>(() => ActiveWorkoutFacade(
+      activeExerciseFacade: locator<IActiveExerciseFacade>(),
+      dataSource: locator<IActiveWorkoutDataSource>()));
 }
 
 void initBloc() {
@@ -61,4 +81,6 @@ void initBloc() {
         workoutFacade: locator<IWorkoutFacade>(),
       ));
   locator.registerFactory<WorkoutRunningBloc>(() => WorkoutRunningBloc());
+  locator.registerFactory<ActiveWorkoutHubCubit>(() => ActiveWorkoutHubCubit(
+      activeWorkoutFacade: locator<IActiveWorkoutFacade>()));
 }
