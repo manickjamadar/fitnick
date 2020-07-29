0e7fa2b527fda6376ee2c425123929990cf7321d
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-29 11:42:31 +0530

    active workout edit option enabled

--- lib/presentation/screens/home/widgets/active_workout/active_workout_item.dart
+++ lib/presentation/screens/home/widgets/active_workout/active_workout_item.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:fitnick/domain/active_workout/models/active_workout.dart';
+import 'package:fitnick/presentation/core/helpers/show_message.dart';
+import 'package:fitnick/presentation/screens/active_workout_form/active_workout_form_screen.dart';
 import 'package:flutter/material.dart';
 
 class ActiveWorkoutItem extends StatelessWidget {
@@ -12,7 +15,40 @@ class ActiveWorkoutItem extends StatelessWidget {
       color: Colors.amber,
       child: ListTile(
         title: Text(activeWorkout.name.safeValue),
+        trailing: PopupMenuButton(
+          onSelected: (value) {
+            if (value == 0) {
+              _onEdit(context);
+            } else if (value == 1) {
+              _onDelete();
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(
+                child: Text("Edit"),
+                value: 0,
+              ),
+              PopupMenuItem(
+                child: Text("Delete"),
+                value: 1,
+              ),
+            ];
+          },
+        ),
       ),
     );
   }
+
+  void _onEdit(BuildContext context) async {
+    final String message = await Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) =>
+          ActiveWorkoutFormScreen.generateRoute(Some(activeWorkout)),
+    ));
+    if (message != null) {
+      showMessage(context, message: message, type: SuccessMessage());
+    }
+  }
+
+  void _onDelete() {}
 }
