a5ea450361752e408d407dc8438a4cef764884d7
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-07-31 11:01:24 +0530

    active exercise removed event logic added

--- lib/application/active_workout/active_workout_form/active_workout_form_cubit.dart
+++ lib/application/active_workout/active_workout_form/active_workout_form_cubit.dart
@@ -4,6 +4,7 @@ import 'package:fitnick/domain/active_exercise/facade/i_active_exercise_facade.d
 import 'package:fitnick/domain/active_exercise/models/active_exercise.dart';
 import 'package:fitnick/domain/active_workout/facade/i_active_workout_facade.dart';
 import 'package:fitnick/domain/active_workout/models/active_workout.dart';
+import 'package:fitnick/domain/core/unique_id.dart';
 import 'package:fitnick/domain/exercise/models/exercise.dart';
 import 'package:fitnick/domain/workout/failure/workout_failure.dart';
 import 'package:fitnick/domain/workout/value_object/workout_name.dart';
@@ -20,6 +21,7 @@ class ActiveWorkoutFormCubit extends Cubit<ActiveWorkoutFormState> {
       : super(ActiveWorkoutFormState.initial());
 
   List<ActiveExercise> _creatableActiveExercises = [];
+  List<ActiveExercise> _removableActiveExercises = [];
 
   //events
   Future<void> init(
@@ -49,8 +51,14 @@ class ActiveWorkoutFormCubit extends Cubit<ActiveWorkoutFormState> {
 
   Future<void> activeExerciseRemoved(
       {@required ActiveExercise activeExercise}) async {
-    //TODO: implement logic
-    throw UnimplementedError();
+    _removeActiveExercise(activeExercise);
+    final newActiveExercises = state.activeWorkout.activeExercises
+        .where((e) => e.id != activeExercise.id)
+        .toList();
+    emit(state.copyWith(
+        addStatus: none(),
+        activeWorkout:
+            state.activeWorkout.copyWith(activeExercises: newActiveExercises)));
   }
 
   Future<void> activeExerciseReordered(
@@ -64,6 +72,7 @@ class ActiveWorkoutFormCubit extends Cubit<ActiveWorkoutFormState> {
     Either<WorkoutFailure, Unit> failureOrSuccess;
     if (state.activeWorkout.isValid) {
       await _saveCreatableActiveExercises();
+      await _removeRemovableActiveExercises();
       failureOrSuccess = state.isEditing
           ? await activeWorkoutFacade.update(state.activeWorkout)
           : await activeWorkoutFacade.create(state.activeWorkout);
@@ -74,10 +83,37 @@ class ActiveWorkoutFormCubit extends Cubit<ActiveWorkoutFormState> {
         addStatus: optionOf(failureOrSuccess)));
   }
 
+  void _removeActiveExercise(ActiveExercise activeExercise) {
+    if (state.isEditing && !_isPresentInCreatable(activeExercise)) {
+      _addRemovableActiveExercise(activeExercise);
+    } else {
+      _removeFromCreatableActiveExercises(activeExercise.id);
+    }
+  }
+
+  bool _isPresentInCreatable(ActiveExercise activeExercise) {
+    return _creatableActiveExercises.any((e) => e.id == activeExercise.id);
+  }
+
   ActiveExercise _addCreatableActiveExercise(Exercise exercise) {
     final activeExercise = ActiveExercise.fromExercise(exercise);
     _creatableActiveExercises.add(activeExercise);
     return activeExercise;
+  }
+
+  void _removeFromCreatableActiveExercises(UniqueId activeExerciseId) {
+    _creatableActiveExercises.removeWhere((e) => e.id == activeExerciseId);
+  }
+
+  ActiveExercise _addRemovableActiveExercise(ActiveExercise activeExercise) {
+    _removableActiveExercises.add(activeExercise);
+    return activeExercise;
+  }
+
+  Future<void> _removeRemovableActiveExercises() async {
+    _removableActiveExercises.forEach((activeExercise) async {
+      await activeExerciseFacade.delete(activeExercise.id);
+    });
   }
 
   Future<void> _saveCreatableActiveExercises() async {
