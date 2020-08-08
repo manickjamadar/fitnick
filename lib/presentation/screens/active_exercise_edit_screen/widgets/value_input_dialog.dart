c8bf6db16234de2f2e80e5c29ab0f73a7a07c043
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-08 15:01:10 +0530

    value input dialog created

---
+++ lib/presentation/screens/active_exercise_edit_screen/widgets/value_input_dialog.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ValueInputDialog extends StatefulWidget {
+  final String value;
+  final String title;
+  const ValueInputDialog({Key key, @required this.value, @required this.title})
+      : super(key: key);
+
+  @override
+  _ValueInputDialogState createState() => _ValueInputDialogState();
+}
+
+class _ValueInputDialogState extends State<ValueInputDialog> {
+  String _value;
+  @override
+  void initState() {
+    super.initState();
+    _value = widget.value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        widget.title,
+        textAlign: TextAlign.center,
+      ),
+      content: TextFormField(
+        autofocus: true,
+        initialValue: _value,
+        onChanged: (value) => setState(() {
+          _value = value;
+        }),
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(border: OutlineInputBorder()),
+      ),
+      actions: [
+        RaisedButton(
+          child: Text("cancel"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        RaisedButton(
+          child: Text("Ok"),
+          onPressed: () {
+            Navigator.pop(context, _value);
+          },
+        ),
+      ],
+    );
+  }
+}
