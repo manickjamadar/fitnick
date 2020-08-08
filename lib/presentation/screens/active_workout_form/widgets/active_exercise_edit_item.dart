d05e4ff5e1c4c8715ba6145acf39972a703d4488
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-08 16:29:11 +0530

    add edit exercise screen and update exercise logic in active exercise edit item on tap

--- lib/presentation/screens/active_workout_form/widgets/active_exercise_edit_item.dart
+++ lib/presentation/screens/active_workout_form/widgets/active_exercise_edit_item.dart
@@ -1,5 +1,8 @@
+import 'package:fitnick/application/active_workout/active_workout_form/active_workout_form_cubit.dart';
 import 'package:fitnick/domain/active_exercise/models/active_exercise.dart';
+import 'package:fitnick/presentation/screens/active_exercise_edit_screen/active_exercise_edit_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ActiveExerciseEditItem extends StatelessWidget {
   final ActiveExercise activeExercise;
@@ -11,6 +14,7 @@ class ActiveExerciseEditItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () => _onItemTap(context),
         title: Text(activeExercise.exercise.name.safeValue),
         subtitle: Text("${activeExercise.sets.length} sets"),
         trailing: IconButton(
@@ -23,5 +27,19 @@ class ActiveExerciseEditItem extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _onItemTap(BuildContext context) async {
+    final ActiveExercise updatedActiveExercise = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => ActiveExerciseEditScreen(
+                  activeExercise: activeExercise,
+                )));
+    if (updatedActiveExercise == null) {
+      return;
+    }
+    BlocProvider.of<ActiveWorkoutFormCubit>(context)
+        .activeExerciseUpdated(activeExercise: updatedActiveExercise);
   }
 }
