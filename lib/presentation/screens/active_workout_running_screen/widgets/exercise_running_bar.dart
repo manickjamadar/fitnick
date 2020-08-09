93026db66aaf789c5124712a5220be3fbfdc4953
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-09 10:17:39 +0530

    exercise running bar created

---
+++ lib/presentation/screens/active_workout_running_screen/widgets/exercise_running_bar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ExerciseRunningBar extends StatelessWidget {
+  final int totalExercise;
+  final int currentExerciseIndex;
+  final double height;
+  final Color activeColor;
+  const ExerciseRunningBar({
+    Key key,
+    @required this.totalExercise,
+    @required this.currentExerciseIndex,
+    this.height = 8,
+    this.activeColor = Colors.blue,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final radius = BorderRadius.circular(10);
+    return Stack(
+      children: [
+        Container(
+          height: height,
+          decoration:
+              BoxDecoration(color: Colors.grey[200], borderRadius: radius),
+        ),
+        LayoutBuilder(
+          builder: (_, constraints) {
+            final width = constraints.biggest.width;
+            return Container(
+              width: width * progress,
+              height: height,
+              decoration: BoxDecoration(
+                  borderRadius: radius,
+                  gradient: LinearGradient(
+                      colors: [activeColor.withOpacity(0.4), activeColor])),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  double get progress {
+    double result = (currentExerciseIndex + 1) / totalExercise;
+    if (result < 0) {
+      result = 0;
+    } else if (result > 1) {
+      result = 1;
+    }
+    return result;
+  }
+}
