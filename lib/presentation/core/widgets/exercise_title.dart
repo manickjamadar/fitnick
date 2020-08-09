db52dc502237666f0e044327be26114c1e55cfe9
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-09 09:15:31 +0530

    capitalized exercise title

--- lib/presentation/core/widgets/exercise_title.dart
+++ lib/presentation/core/widgets/exercise_title.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "../helpers/string_extension.dart";
 
 class ExerciseTitle extends StatelessWidget {
   final String title;
@@ -7,7 +8,7 @@ class ExerciseTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-      title,
+      title.capitalize(),
       textAlign: TextAlign.center,
       style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
     );
