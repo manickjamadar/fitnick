4a970ca86245ccabb1663f00b3f6c050a7630c58
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-12 11:39:55 +0530

    updated asset music in data music

--- lib/infrastructure/music/data_source/data.dart
+++ lib/infrastructure/music/data_source/data.dart
@@ -2,7 +2,14 @@ import 'package:fitnick/domain/core/unique_id.dart';
 import 'package:fitnick/domain/music/models/music.dart';
 
 List<Music> allMusic = [
-  Music(id: UniqueId(), name: "Song 1", path: "song1.mp3"),
-  Music(id: UniqueId(), name: "Song 2", path: "song1.mp3"),
-  Music(id: UniqueId(), name: "Song 3", path: "song1.mp3"),
+  Music(
+    id: UniqueId(),
+    name: "Cartoon - On & On",
+    path: "cartoon_on_and_on.mp3",
+  ),
+  Music(
+    id: UniqueId(),
+    name: "Itro & Tobu",
+    path: "itro_and_tobu.mp3",
+  ),
 ];
