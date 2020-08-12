e8e18ee10959dded789e3d4c8180f0e1a2957f88
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-12 11:40:17 +0530

    add on pressed method in music item

--- lib/presentation/screens/music_center_screen/widgets/music_item.dart
+++ lib/presentation/screens/music_center_screen/widgets/music_item.dart
@@ -3,18 +3,29 @@ import 'package:flutter/material.dart';
 
 class MusicItem extends StatelessWidget {
   final Music music;
+  final void Function() onPressed;
 
-  const MusicItem({Key key, @required this.music}) : super(key: key);
+  const MusicItem({Key key, @required this.music, this.onPressed})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Icon(music.isPlaying ? Icons.pause : Icons.play_arrow),
+      child: InkWell(
+        onTap: () {
+          if (onPressed != null) {
+            onPressed();
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: music.isPlaying ? Colors.red : Colors.blue,
+              child: Icon(music.isPlaying ? Icons.stop : Icons.play_arrow,
+                  color: Colors.white),
+            ),
+            title: Text(music.name),
           ),
-          title: Text(music.name),
         ),
       ),
     );
