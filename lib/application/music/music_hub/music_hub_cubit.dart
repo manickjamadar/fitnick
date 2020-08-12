04b1f00d0cf9454d20fb1f7b4b42d16cab15e1b6
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-12 11:40:06 +0530

    added play and stop logic in music hub cubit

--- lib/application/music/music_hub/music_hub_cubit.dart
+++ lib/application/music/music_hub/music_hub_cubit.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:bloc/bloc.dart';
 import 'package:fitnick/domain/music/facade/i_music_facade.dart';
 import 'package:fitnick/domain/music/models/music.dart';
@@ -7,13 +9,60 @@ part 'music_hub_state.dart';
 part 'music_hub_cubit.freezed.dart';
 
 class MusicHubCubit extends Cubit<MusicHubState> {
+  static AudioCache cachePlayer = AudioCache(prefix: "music/");
   final IMusicFacade musicFacade;
   MusicHubCubit({@required this.musicFacade}) : super(MusicHubState.loading()) {
     _init();
   }
 
+  AudioPlayer player;
+
+  Future<void> _stopRunningMusic() async {
+    if (player == null) return;
+    if (player.state == AudioPlayerState.PLAYING) {
+      await player.stop();
+    }
+  }
+
+  List<Music> _resetMusicPlayingList(List<Music> musicList) {
+    return musicList.map((m) => m.copyWith(isPlaying: false)).toList();
+  }
+
   //events
   void _init() {
-    emit(MusicHubState.loaded(musics: musicFacade.getAll()));
+    final musicList = musicFacade.getAll();
+    cachePlayer.loadAll(musicList.map((e) => e.path).toList());
+    emit(MusicHubState.loaded(musics: musicList));
+  }
+
+  Future<void> play(Music music) async {
+    _stopRunningMusic();
+    player = await cachePlayer.loop(music.path);
+    state.maybeWhen(
+        orElse: () {},
+        loaded: (List<Music> musicList) {
+          final newMusicList = _resetMusicPlayingList(musicList)
+              .map(
+                  (m) => m.id == music.id ? music.copyWith(isPlaying: true) : m)
+              .toList();
+          emit(MusicHubState.loaded(musics: newMusicList));
+        });
+  }
+
+  void stop() {
+    _stopRunningMusic();
+    state.maybeWhen(
+        orElse: () {},
+        loaded: (List<Music> musicList) {
+          emit(MusicHubState.loaded(musics: _resetMusicPlayingList(musicList)));
+        });
+  }
+
+  @override
+  Future<void> close() async {
+    await _stopRunningMusic();
+    player?.dispose();
+    cachePlayer?.clearCache();
+    return super.close();
   }
 }
