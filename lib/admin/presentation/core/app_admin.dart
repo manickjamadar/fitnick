800de8d584c38f6c07ece5d418a2d2ea40272243
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-17 18:14:05 +0530

    basic admin home screen created

--- lib/admin/presentation/core/app_admin.dart
+++ lib/admin/presentation/core/app_admin.dart
@@ -1,19 +1,12 @@
+import 'package:fitnick/admin/presentation/screen/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class AdminApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fitnick Admin'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
