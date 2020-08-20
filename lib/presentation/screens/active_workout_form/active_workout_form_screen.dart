8ef642fdbd9f4d96fd8064a2a08ce9186203d59d
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-20 10:47:13 +0530

    Revert "added workout create and update with coin"
    This reverts commit 755dace32ca5f487f815e208fe95d14062a11c35.

--- lib/presentation/screens/active_workout_form/active_workout_form_screen.dart
+++ lib/presentation/screens/active_workout_form/active_workout_form_screen.dart
@@ -1,13 +1,10 @@
 import 'package:dartz/dartz.dart';
-import 'package:fitnick/application/account/account_manager/account_manager_cubit.dart';
 import 'package:fitnick/application/active_workout/active_workout_form/active_workout_form_cubit.dart';
 import 'package:fitnick/domain/active_workout/models/active_workout.dart';
 import 'package:fitnick/presentation/screens/active_workout_form/widgets/active_workout_form_handler.dart';
-import 'package:fitnick/presentation/screens/store_screen/store_screen.dart';
 import 'package:fitnick/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import "../../../application/account/account_hub/account_hub_cubit.dart";
 
 class ActiveWorkoutFormScreen extends StatelessWidget {
   final String title;
@@ -16,20 +13,12 @@ class ActiveWorkoutFormScreen extends StatelessWidget {
   static const routeName = "/active_workout_form";
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AccountManagerCubit, AccountManagerState>(
-      listener: (_, state) {
-        state.maybeWhen(
-            orElse: () {},
-            spendSuccessFul: (coin) => _onWorkoutSave(context),
-            inSufficientBalance: () => _onInSufficientBalance(context));
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-          actions: <Widget>[buildSaveButton(context)],
-        ),
-        body: ActiveWorkoutFormHandler(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        actions: <Widget>[buildSaveButton(context)],
       ),
+      body: ActiveWorkoutFormHandler(),
     );
   }
 
@@ -37,39 +26,22 @@ class ActiveWorkoutFormScreen extends StatelessWidget {
     return BlocBuilder<ActiveWorkoutFormCubit, ActiveWorkoutFormState>(
       builder: (_, state) => IconButton(
         onPressed: !state.isAdding && state.activeWorkout.isValid
-            ? () => _onCheckButtonPressed(context)
+            ? () => onWorkoutSave(context)
             : null,
         icon: Icon(Icons.check),
       ),
     );
   }
 
-  void _onInSufficientBalance(BuildContext context) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (_) => StoreScreen.generateRoute(context)));
-  }
-
-  void _onCheckButtonPressed(BuildContext context) {
-    BlocProvider.of<AccountManagerCubit>(context).spend(ActiveWorkout.price);
-  }
-
-  void _onWorkoutSave(BuildContext context) {
+  void onWorkoutSave(BuildContext context) {
     BlocProvider.of<ActiveWorkoutFormCubit>(context).saved();
   }
 
   static Widget generateRoute(
       BuildContext context, Option<ActiveWorkout> activeWorkoutOption) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ActiveWorkoutFormCubit>(
-          create: (_) => locator<ActiveWorkoutFormCubit>()
-            ..init(activeWorkoutOption: activeWorkoutOption),
-        ),
-        BlocProvider<AccountManagerCubit>(
-          create: (_) => AccountManagerCubit(
-              accountHubCubit: BlocProvider.of<AccountHubCubit>(context)),
-        )
-      ],
+    return BlocProvider<ActiveWorkoutFormCubit>(
+      create: (_) => locator<ActiveWorkoutFormCubit>()
+        ..init(activeWorkoutOption: activeWorkoutOption),
       child: ActiveWorkoutFormScreen(
           title: activeWorkoutOption.fold(
               () => "Add Workout", (a) => "Edit Workout")),
