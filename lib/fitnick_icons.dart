5638edb4d2467917803d5fa985f529dbff0b867b
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-23 09:46:56 +0530

    added custom fitnick icons

---
+++ lib/fitnick_icons.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class FitnickIcons {
+  FitnickIcons._();
+
+  static const _kFontFam = 'Fitnick';
+  static const _kFontPkg = null;
+
+  static const IconData logo =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData flash =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData music =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData voice_off =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData weight =
+      IconData(0xf5cd, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData workout = Icons.group_work;
+  static const IconData exercise = Icons.fitness_center;
+  static const IconData progression = Icons.trending_up;
+  static const IconData store = Icons.shopping_basket;
+  static const IconData voice = Icons.record_voice_over;
+  static const IconData filter = Icons.filter_list;
+  static const IconData timer = Icons.av_timer;
+  static const IconData upload = Icons.save_alt;
+  static const IconData close = Icons.close;
+  static const IconData exerciseSet = Icons.refresh;
+}
