4c1c1f0ddbbd67b9544950490ff98d6c7667fac4
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-23 17:46:34 +0530

    implemented tab cubit ui and page view in home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,15 +1,17 @@
+import 'package:dartz/dartz.dart' as dartz;
 import 'package:fitnick/application/exercise/exercise_hub/exercise_hub_bloc.dart';
 import 'package:fitnick/application/exercise/filtered_exercise/filtered_exercise_bloc.dart';
 import 'package:fitnick/application/music/music_hub/music_hub_cubit.dart';
+import 'package:fitnick/application/tab/tab_cubit.dart';
+import 'package:fitnick/domain/page_tab/models/page_tab.dart';
 import 'package:fitnick/fitnick_icons.dart';
-import 'package:fitnick/presentation/core/my_icons.dart';
-import 'package:fitnick/presentation/core/widgets/balance_view.dart';
+import 'package:fitnick/presentation/core/helpers/show_message.dart';
+import 'package:fitnick/presentation/screens/active_workout_form/active_workout_form_screen.dart';
+import 'package:fitnick/presentation/screens/exercise_form/exercise_form_screen.dart';
 import 'package:fitnick/presentation/screens/home/tabs/exercise_tab.dart';
 import 'package:fitnick/presentation/screens/home/tabs/progresstion_tab.dart';
 import 'package:fitnick/presentation/screens/home/tabs/workout_tab.dart';
-import 'package:fitnick/presentation/screens/home/widgets/add_buttons.dart';
 import 'package:fitnick/presentation/screens/music_center_screen/music_center_screen.dart';
-import 'package:fitnick/presentation/screens/store_screen/store_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -32,85 +34,89 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
+    return BlocBuilder<TabCubit, TabState>(builder: (_, state) {
+      final PageTab currentPageTab = state.tabs[state.currentTabIndex];
+      return Scaffold(
         backgroundColor: Colors.white,
-        textTheme: Theme.of(context).textTheme,
-        iconTheme: IconThemeData(color: Colors.black),
-        elevation: 0,
-        leading: Icon(FitnickIcons.logo, color: Theme.of(context).primaryColor),
-        title: Text(
-          "Workouts",
-          style: Theme.of(context).textTheme.bodyText1,
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(FitnickIcons.store, color: Colors.green),
-            onPressed: () {},
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          textTheme: Theme.of(context).textTheme,
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0,
+          leading:
+              Icon(FitnickIcons.logo, color: Theme.of(context).primaryColor),
+          title: Text(
+            currentPageTab.title,
+            style: Theme.of(context).textTheme.bodyText1,
           ),
-          IconButton(
-            icon: Icon(
-              FitnickIcons.music,
-              size: 20,
-              color: Colors.red,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(FitnickIcons.store, color: Colors.green),
+              onPressed: () {},
             ),
-            onPressed: () => _onHeadSetTap(context),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        icon: Icon(Icons.add, color: Colors.white),
-        label: Text("Add Workout",
-            style: Theme.of(context)
-                .textTheme
-                .bodyText1
-                .copyWith(color: Colors.white)),
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        elevation: 40,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Expanded(
-              child: IconButton(
-                icon: Icon(FitnickIcons.workout),
-                onPressed: () {},
+            IconButton(
+              icon: Icon(
+                FitnickIcons.music,
+                size: 20,
+                color: Colors.red,
               ),
-            ),
-            Expanded(child: Text("")),
-            Expanded(
-              child: IconButton(
-                icon: Icon(FitnickIcons.exercise),
-                onPressed: () {},
-              ),
-            ),
+              onPressed: () => _onHeadSetTap(context),
+            )
           ],
         ),
-      ),
-      body: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: IndexedStack(
-          index: currentTab,
-          children: <Widget>[
-            WorkoutTab(),
-            BlocProvider<FilteredExerciseBloc>(
-              create: (_) => FilteredExerciseBloc(
-                  exerciseHubBloc: BlocProvider.of<ExerciseHubBloc>(context)),
-              child: ExerciseTab(),
-            ),
-            ProgressionTab()
-          ],
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            if (state.currentTabIndex == 0) {
+              _onAddWorkout(context);
+            } else {
+              _onAddExercise(context);
+            }
+          },
+          icon: Icon(Icons.add, color: Colors.white),
+          label: Text(
+              state.currentTabIndex == 0 ? "Add Workout" : "Add Exercise",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  .copyWith(color: Colors.white)),
+          backgroundColor: Theme.of(context).primaryColor,
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: state.currentTabIndex,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: (newTabIndex) => _onTabSelect(context, newTabIndex),
+          items: state.tabs
+              .map((pageTab) => BottomNavigationBarItem(
+                  icon: Icon(pageTab.iconData), title: Text(pageTab.title)))
+              .toList(),
+        ),
+        body: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: PageView(
+            controller: state.pageController,
+            onPageChanged: (newPageIndex) {
+              if (state.currentTabIndex != newPageIndex) {
+                _onTabSelect(context, newPageIndex);
+              }
+            },
+            children: [
+              WorkoutTab(),
+              BlocProvider<FilteredExerciseBloc>(
+                create: (_) => FilteredExerciseBloc(
+                    exerciseHubBloc: BlocProvider.of<ExerciseHubBloc>(context)),
+                child: ExerciseTab(),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 
   void _onHeadSetTap(BuildContext context) {
@@ -121,12 +127,30 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
         ));
   }
 
-  void _onCoinIconTap(BuildContext context) {
-    Navigator.push(
+  void _onTabSelect(BuildContext context, int newTabIndex) {
+    BlocProvider.of<TabCubit>(context).changeTab(newTabIndex);
+  }
+
+  void _onAddExercise(BuildContext context) async {
+    String message = await Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (_) => StoreScreen.generateRoute(context),
-        ));
+            builder: (_) =>
+                ExerciseFormScreen.generateRoute(context, dartz.none())));
+    if (message != null) {
+      showMessage(context, message: message, type: SuccessMessage());
+    }
+  }
+
+  void _onAddWorkout(BuildContext context) async {
+    String message = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) =>
+                ActiveWorkoutFormScreen.generateRoute(context, dartz.none())));
+    if (message != null) {
+      showMessage(context, message: message, type: SuccessMessage());
+    }
   }
 
   @override
