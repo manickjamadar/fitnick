dfb800b4156a809c48ff1ed57b4a971db94f89b2
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-24 12:22:26 +0530

    added go store and go music center in fitnick actions

--- lib/presentation/core/fitnick_actions/fitnick_actions.dart
+++ lib/presentation/core/fitnick_actions/fitnick_actions.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:fitnick/presentation/core/helpers/show_message.dart';
 import 'package:fitnick/presentation/screens/active_workout_form/active_workout_form_screen.dart';
 import 'package:fitnick/presentation/screens/exercise_form/exercise_form_screen.dart';
+import 'package:fitnick/presentation/screens/music_center_screen/music_center_screen.dart';
+import 'package:fitnick/presentation/screens/store_screen/store_screen.dart';
 import 'package:flutter/material.dart';
 
 class FitnickActions {
@@ -28,5 +30,18 @@ class FitnickActions {
     if (message != null) {
       showMessage(context, message: message, type: SuccessMessage());
     }
+  }
+
+  void goMusicCenter() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => MusicCenterScreen.generateRoute(),
+        ));
+  }
+
+  void goStore() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (_) => StoreScreen.generateRoute(context)));
   }
 }
