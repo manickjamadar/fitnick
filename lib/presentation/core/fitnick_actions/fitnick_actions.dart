3689376a7de5cd4beea1b9dd41579d334cb7edd1
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-24 12:14:31 +0530

    basic fitnick action created with create exercise and create workout method

---
+++ lib/presentation/core/fitnick_actions/fitnick_actions.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:fitnick/presentation/core/helpers/show_message.dart';
+import 'package:fitnick/presentation/screens/active_workout_form/active_workout_form_screen.dart';
+import 'package:fitnick/presentation/screens/exercise_form/exercise_form_screen.dart';
+import 'package:flutter/material.dart';
+
+class FitnickActions {
+  final BuildContext context;
+
+  const FitnickActions(this.context);
+
+  void onCreateExerciseButtonPressed() async {
+    String message = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => ExerciseFormScreen.generateRoute(context, none())));
+    if (message != null) {
+      showMessage(context, message: message, type: SuccessMessage());
+    }
+  }
+
+  void onCreateWorkotuButtonPressed() async {
+    String message = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) =>
+                ActiveWorkoutFormScreen.generateRoute(context, none())));
+    if (message != null) {
+      showMessage(context, message: message, type: SuccessMessage());
+    }
+  }
+}
