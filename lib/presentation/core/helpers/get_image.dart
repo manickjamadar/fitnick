27cf1951bc7d1ea4e51553f790aa43c2552cfe70
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-25 11:21:28 +0530

    image quality parameter added in get image method

--- lib/presentation/core/helpers/get_image.dart
+++ lib/presentation/core/helpers/get_image.dart
@@ -1,5 +1,5 @@
 import 'package:image_picker/image_picker.dart';
 
-Future<PickedFile> pickNewImage() async {
-  return ImagePicker().getImage(source: ImageSource.gallery);
+Future<PickedFile> pickNewImage({int imageQuality = 100}) async {
+  return ImagePicker().getImage(source: ImageSource.gallery, imageQuality: 100);
 }
