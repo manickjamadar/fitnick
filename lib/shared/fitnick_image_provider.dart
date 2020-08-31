5dc3dada4e6e17893cdd399e04fc82ae70ac1d4d
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-08-31 16:47:24 +0530

    some dialog images added

--- lib/shared/fitnick_image_provider.dart
+++ lib/shared/fitnick_image_provider.dart
@@ -1,4 +1,7 @@
 class FitnickImageProvider {
   static const no_exercise = "assets/images/no_exercise.png";
   static const no_workout = "assets/images/no_workout.png";
+  static const purchase_success = "assets/images/purchase_success.png";
+  static const credit_success = "assets/images/credit_success.png";
+  static const quit_workout = "assets/images/quit_workout.png";
 }
