504d89edf41f2e019a398635d0f82984ff62ee51
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-02 10:16:05 +0530

    timer formatter method crerated

--- lib/application/core/helpers/time_formatter.dart
+++ lib/application/core/helpers/time_formatter.dart
@@ -11,3 +11,41 @@ String formatTime(Duration duration) {
     return "$twoDigitHours $twoDigitMinutes $twoDigitSeconds";
   }
 }
+
+String formatTimeInWords(Duration duration) {
+  final formatter = DurationFormatter.format(duration);
+  String format = "${formatter.seconds} Secs";
+  if (duration.inMinutes > 0) {
+    format = "${formatter.minutes} Mins " + format;
+  }
+  if (duration.inHours > 0) {
+    format = "${formatter.hours} Hours " + format;
+  }
+  return format;
+}
+
+String formatTimeInNums(Duration duration) {
+  final formatter = DurationFormatter.format(duration);
+  String format = "${formatter.seconds}";
+  if (duration.inMinutes > 0) {
+    format = "${formatter.minutes}:" + format;
+  }
+  if (duration.inHours > 0) {
+    format = "${formatter.hours}:" + format;
+  }
+  return format;
+}
+
+class DurationFormatter {
+  final String hours;
+  final String minutes;
+  final String seconds;
+  const DurationFormatter._(this.hours, this.minutes, this.seconds);
+  factory DurationFormatter.format(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitHours = twoDigits(duration.inHours);
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return DurationFormatter._(twoDigitHours, twoDigitMinutes, twoDigitSeconds);
+  }
+}
