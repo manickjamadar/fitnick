8008dfb73f85e6750ee827e748579b05653db611
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-03 16:20:34 +0530

    new filtered exercise list view created from exercise list view

--- lib/presentation/screens/home/widgets/exercise/filtered_exerise_list_view.dart
+++ lib/presentation/screens/home/widgets/exercise/filtered_exerise_list_view.dart
@@ -2,25 +2,29 @@ import 'package:fitnick/domain/exercise/models/exercise.dart';
 import 'package:fitnick/presentation/core/fitnick_actions/fitnick_actions.dart';
 import 'package:fitnick/presentation/core/widgets/action_button.dart';
 import 'package:fitnick/presentation/core/widgets/actor_dialog.dart';
+import 'package:fitnick/presentation/core/widgets/exercise_filter_controller.dart';
 import 'package:fitnick/presentation/core/widgets/exercise_tile.dart';
 import 'package:fitnick/presentation/core/widgets/not_found_action.dart';
-import 'package:fitnick/presentation/core/widgets/search_bar.dart';
-import "../../../../core/helpers/string_extension.dart";
+import '../../../../core/helpers/string_extension.dart';
 import 'package:fitnick/shared/fitnick_image_provider.dart';
 import 'package:flutter/material.dart';
 
-class ExerciseListView extends StatelessWidget {
+class FilteredExerciseListView extends StatelessWidget {
   final List<Exercise> exercises;
   final bool slidable;
   final bool searchable;
   final String searchValue;
+  final Exercise filteredExercise;
+  final void Function(Exercise newExercise) onExerciseChanged;
   final void Function(String term) onSearch;
 
-  const ExerciseListView(
+  const FilteredExerciseListView(
       {Key key,
       this.slidable = false,
       this.onSearch,
       this.searchValue = "",
+      @required this.filteredExercise,
+      @required this.onExerciseChanged,
       @required this.exercises,
       this.searchable = false})
       : super(key: key);
@@ -35,7 +39,7 @@ class ExerciseListView extends StatelessWidget {
     return ListView.builder(
       itemBuilder: (context, index) {
         if (index == 0 && searchable) {
-          return buildSearchBar(context);
+          return buildListHeader(context);
         }
         final realIndex = index - (searchable ? 1 : 0);
         final exercise = exercises[realIndex];
@@ -64,13 +68,13 @@ class ExerciseListView extends StatelessWidget {
     );
   }
 
-  Widget buildSearchBar(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: SearchBar(
-        value: searchValue,
-        onChanged: onSearch,
-      ),
+  Widget buildListHeader(BuildContext context) {
+    return ExerciseFilterController(
+      exercise: filteredExercise,
+      searchTerm: searchValue,
+      onSearched: onSearch,
+      onFilter: () => FitnickActions(context).goExerciseFilterScreen(),
+      onExerciseChanged: onExerciseChanged,
     );
   }
 
