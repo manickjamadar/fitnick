0d9909a554fe6fcab72f5309cfd02217ca83f01e
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-04 21:16:19 +0530

    added new music

--- lib/infrastructure/music/data_source/data.dart
+++ lib/infrastructure/music/data_source/data.dart
@@ -12,4 +12,46 @@ List<Music> allMusic = [
     name: "Itro & Tobu",
     path: "itro_and_tobu.mp3",
   ),
+  Music(
+      id: UniqueId(),
+      name: "Alan Walker, Sabrina Carpenter & Farruko - On My Way",
+      path: "alan_walker_sabrina_carpenter_farruko.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Alan Walker - Faded",
+      path: "alan_Walker_faded.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Alan Walker - The Spectre",
+      path: "alan_walker_the_spectre.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Dharia -Sugar And Brownies",
+      path: "dharia_sugar_and_brownies.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Ed Sheeran - Shape of You",
+      path: "ed_sheeran_shape_of_you.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Imagine Dragons - Believer",
+      path: "imagine_dragons_believer.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Julius Dreisig & Zeus X Crona - Invisible",
+      path: "julius_dreisig_zeus_crona_invisible.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "LET ME LOVE YOU - Justin Bieber",
+      path: "let_me_love_you.mp3"),
+  Music(id: UniqueId(), name: "Serena - Safari", path: "serena_safari.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Shawn Mendes, Camila Cabello - Señorita",
+      path: "shawn_mendes.mp3"),
+  Music(
+      id: UniqueId(),
+      name: "Ship Wrek & Zookeepers - Ark",
+      path: "ship_wrek_zookeepers.mp3"),
+  Music(id: UniqueId(), name: "Tobu - Hope", path: "tobu_hope.mp3"),
 ];
